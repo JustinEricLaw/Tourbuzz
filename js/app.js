1e16abce5479b058be2f5557d3cccadbcfeaabab
J Law <justin.eric.law@gmail.com>
2015-10-19 23:00:45 -0400

    Add responsiveness, carousel, etc

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  
+  // Owl Carousel Settings
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    responsiveClass: true,
+    lazyLoad:true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      900: {
+        items: 1,
+        nav: true,
+        stagePadding: 100
+      },
+      1200: {
+        items: 1,
+        nav: true,
+        stagePadding: 250
+      }
+    }
+  });
+
+
+  // Remove Carousel Prev/Next, Add Icons
+  $(".owl-prev").text("").addClass("icon-left");
+  $(".owl-next").text("").addClass("icon-right");
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
